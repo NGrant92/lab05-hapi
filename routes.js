f6e8460a4add5547524c284d74ccb76c7b84da00
NGrant <niall.grant1992@gmail.com>
2017-09-25 12:06:00 +0100

    Added new partials and reformatting .hbs files

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 const Donations = require('./app/controllers/donations');
 const Assets = require('./app/controllers/assets');
-const SignUp = require('./app/controllers/signup');
-const Login = require('./app/controllers/login');
 
 //returns this info if called
 module.exports = [
