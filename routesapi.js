7b031f131a4f70db8d218f1d12c792172d11b30c
NGrant <niall.grant1992@gmail.com>
2017-10-17 12:30:42 +0100

    Added route for candidatesapi.js/findOne

--- routesapi.js
+++ routesapi.js
@@ -5,5 +5,10 @@ module.exports = [
     method: "GET",
     path: "/api/candidates",
     config: CandidatesApi.find
+  },
+  {
+    method: "GET",
+    path: "/api/candidates/{id}",
+    config: CandidatesApi.findOne
   }
 ];
