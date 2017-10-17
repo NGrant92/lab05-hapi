15ff771c89f3492288e7cca26c8fc80a87c15425
NGrant <niall.grant1992@gmail.com>
2017-10-17 12:53:49 +0100

    Added findOne test function. Added 1 POST and 2 DELETE routes to routesapi.js

--- routesapi.js
+++ routesapi.js
@@ -10,5 +10,20 @@ module.exports = [
     method: "GET",
     path: "/api/candidates/{id}",
     config: CandidatesApi.findOne
+  },
+  {
+    method: "POST",
+    path: "/api/candidates",
+    config: CandidatesApi.create
+  },
+  {
+    method: "DELETE",
+    path: "/api/candidates/{id}",
+    config: CandidatesApi.deleteOne
+  },
+  {
+    method: "DELETE",
+    path: "/api/candidates",
+    config: CandidatesApi.deleteAll
   }
 ];
