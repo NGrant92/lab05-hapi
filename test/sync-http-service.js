482822321fa549b808fe2550c6ae100146fb3496
NGrant <niall.grant1992@gmail.com>
2017-11-07 11:35:34 +0000

    added test classes and usersapi.js

---
+++ test/sync-http-service.js
@@ -0,0 +1,30 @@
+let request = require('sync-request');
+
+class SyncHttpService {
+
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  get(url) {
+    let returnedObj = null;
+    let res = request('GET', this.baseUrl + url);
+    if (res.statusCode < 300) {
+      returnedObj = JSON.parse(res.getBody('utf8'));
+    }
+
+    return returnedObj;
+  }
+
+  post(url, obj) {
+    let returnedObj = null;
+    let res = request('POST', this.baseUrl + url, { json: obj });
+    if (res.statusCode < 300) {
+      returnedObj = JSON.parse(res.getBody('utf8'));
+    }
+
+    return returnedObj;
+  }
+}
+
+module.exports = SyncHttpService;
