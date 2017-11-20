00777076f9f4368b98214736b112ce16fed133f1
NGrant <niall.grant1992@gmail.com>
2017-11-20 13:26:22 +0000

    bcrypt authentication is used to encrypt user's passwords and to validate their password when logging in

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,77 +1,87 @@
-'use strict';
-const User = require('../models/user');
-const Joi = require('joi');
+"use strict";
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const User = require("../models/user");
+const Joi = require("joi");
 
 exports.main = {
   auth: false,
-  handler: function (request, reply) {
-    reply.view('main', { title: 'Welcome to Donations' });
-  },
+  handler: function(request, reply) {
+    reply.view("main", { title: "Welcome to Donations" });
+  }
 };
 
 exports.signup = {
   auth: false,
-  handler: function (request, reply) {
-    reply.view('signup', { title: 'Sign up for Donations' });
-  },
+  handler: function(request, reply) {
+    reply.view("signup", { title: "Sign up for Donations" });
+  }
 };
 
 exports.login = {
   auth: false,
-  handler: function (request, reply) {
-    reply.view('login', { title: 'Login to Donations' });
-  },
+  handler: function(request, reply) {
+    reply.view("login", { title: "Login to Donations" });
+  }
 };
 
 exports.logout = {
   auth: false,
-  handler: function (request, reply) {
+  handler: function(request, reply) {
     request.cookieAuth.clear();
-    reply.redirect('/');
-  },
+    reply.redirect("/");
+  }
 };
 
 exports.authenticate = {
-
   auth: false,
 
   validate: {
-
     payload: {
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string().required()
     },
 
     options: {
-      abortEarly: false,
+      abortEarly: false
     },
 
-    failAction: function (request, reply, source, error) {
-      reply.view('login', {
-        title: 'Sign in error',
-        errors: error.data.details,
-      }).code(400);
-    },
-
+    failAction: function(request, reply, source, error) {
+      reply
+        .view("login", {
+          title: "Sign in error",
+          errors: error.data.details
+        })
+        .code(400);
+    }
   },
 
-  handler: function (request, reply) {
+  handler: function(request, reply) {
     const user = request.payload;
-    User.findOne({ email: user.email }).then(foundUser => {
-      if (foundUser && foundUser.password === user.password) {
-        request.cookieAuth.set({
-          loggedIn: true,
-          loggedInUser: user.email,
+    User.findOne({ email: user.email })
+      .then(foundUser => {
+        bcrypt.compare(user.password, foundUser.password, function(
+          err,
+          isValid
+        ) {
+          if (isValid) {
+            request.cookieAuth.set({
+              loggedIn: true,
+              loggedInUser: user.email
+            });
+            reply.redirect("/home");
+          } else {
+            reply.redirect("/signup");
+          }
         });
-        reply.redirect('/home');
-      } else {
-        reply.redirect('/signup');
-      }
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-
+      })
+      .catch(err => {
+        reply.redirect("/signup");
+      });
+  }
 };
 
 exports.userRegister = {
@@ -81,83 +91,104 @@ exports.userRegister = {
     payload: {
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string().required()
     },
 
     options: {
-      abortEarly: false,
+      abortEarly: false
     },
 
-    failAction: function (request, reply, source, error) {
-      reply.view('signup', {
-        title: 'Signup error',
-        errors: error.data.details,
-      }).code(400);
-    },
+    failAction: function(request, reply, source, error) {
+      reply
+        .view("signup", {
+          title: "Signup error",
+          errors: error.data.details
+        })
+        .code(400);
+    }
   },
 
-  handler: function (request, reply) {
+  handler: function(request, reply) {
     const user = new User(request.payload);
+    const plaintextPassword = user.password;
 
-    user.save().then(newUser => {
-      reply.redirect('/login');
-    }).catch(err => {
-      reply.redirect('/');
+    bcrypt.hash(plaintextPassword, saltRounds, function(err, hash) {
+      user.password = hash;
+      return user
+        .save()
+        .then(newUser => {
+          reply.redirect("/login");
+        })
+        .catch(err => {
+          reply.redirect("/");
+        });
     });
-  },
+  }
 };
 
 exports.viewSettings = {
-  handler: function (request, reply) {
+  handler: function(request, reply) {
     const userEmail = request.auth.credentials.loggedInUser;
-    User.findOne({ email: userEmail }).then(foundUser => {
-      reply.view('settings', { title: 'Edit Account Settings', user: foundUser });
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
+    User.findOne({ email: userEmail })
+      .then(foundUser => {
+        reply.view("settings", {
+          title: "Edit Account Settings",
+          user: foundUser
+        });
+      })
+      .catch(err => {
+        reply.redirect("/");
+      });
+  }
 };
 
 exports.updateSettings = {
-
   validate: {
-
     payload: {
       firstName: Joi.string().required(),
       lastName: Joi.string().required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string().required()
     },
 
     options: {
-      abortEarly: false,
+      abortEarly: false
     },
 
-    failAction: function (request, reply, source, error) {
-      reply.view('signup', {
-        title: 'Update error',
-        errors: error.data.details,
-      }).code(400);
-    },
-
+    failAction: function(request, reply, source, error) {
+      reply
+        .view("signup", {
+          title: "Update error",
+          errors: error.data.details
+        })
+        .code(400);
+    }
   },
 
-  handler: function (request, reply) {
+  handler: function(request, reply) {
     const editedUser = request.payload;
     const loggedInUserEmail = request.auth.credentials.loggedInUser;
 
-    User.findOne({ email: loggedInUserEmail }).then(user => {
-      user.firstName = editedUser.firstName;
-      user.lastName = editedUser.lastName;
-      user.email = editedUser.email;
-      user.password = editedUser.password;
-      return user.save();
-    }).then(user => {
-      reply.view('settings', { title: 'Edit Account Settings', user: user });
-    }).catch(err => {
-      reply.redirect('/');
-    });
-  },
-
+    User.findOne({ email: loggedInUserEmail })
+      .then(user => {
+        user.firstName = editedUser.firstName;
+        user.lastName = editedUser.lastName;
+        user.email = editedUser.email;
+        bcrypt.hash(editedUser.password, saltRounds, function(err, hash) {
+          user.password = hash;
+          return user.save();
+        });
+      })
+      .then(user => {
+        reply.view("settings", { title: "Edit Account Settings", user: user });
+      })
+      .catch(err => {
+        reply.redirect("/");
+      });
+  }
 };
